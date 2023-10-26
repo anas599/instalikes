2093b18e98204a322858db682cececdef952496d
anas599 <anas599@yahoo.com>
2023-10-26 17:20:10 +0300

    use env for api url

--- src/App.tsx
+++ src/App.tsx
@@ -30,9 +30,7 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch(
-          "https://anasdevbucket.s3.eu-north-1.amazonaws.com/data.json"
-        );
+        const response = await fetch(import.meta.env.VITE_API);
         const result = await response.json();
         setData(result);
       } catch (error) {
@@ -42,68 +40,80 @@ function App() {
     fetchData();
   }, []);
   // S3 upload
-  const [file, setFile] = useState<File | null>(null);
-  const [url, setUrl] = useState<string>("");
-  // Function to upload file to s3
-  const uploadFile = async () => {
-    const s3 = new S3Client({
-      region: "eu-north-1",
-      credentials: {
-        accessKeyId: "AKIA26KRV4DMO5JL2GXC",
-        secretAccessKey: "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",
-      },
-    });
-    const params = {
-      Bucket: "anasdevbucket",
-      Key: file?.name,
+  // const [file, setFile] = useState<File | null>(null);
+  // const [url, setUrl] = useState<string>("");
+  // // Function to upload file to s3
+  // const uploadFile = async () => {
+  //   const s3 = new S3Client({
+  //     region: "eu-north-1",
+  //     credentials: {
+  //       accessKeyId: "AKIA2",
+  //       secretAccessKey: "Ydv",
+  //     },
+  //   });
+  //   const params = {
+  //     Bucket: "anasdevbucket",
+  //     Key: file?.name,
 
-      Body: file?.body,
-    };
-    // Uploading file to s3
-    const upload = s3.send(new PutObjectCommand(params));
-    //critical part of uploading to s3 depends on the name of the CORS configuration start
-    await upload
-      .then(() => {
-        setUrl(`https://anasdevbucket.s3.eu-north-1.amazonaws.com/data.json`);
-        //critical part end check in Firefox if CORS fails
-        console.log(`File uploaded successfully at ${url}`);
+  //     Body: file?.body,
+  //   };
+  //   // Uploading file to s3
+  //   const upload = s3.send(new PutObjectCommand(params));
+  //   //critical part of uploading to s3 depends on the name of the CORS configuration start
+  //   await upload
+  //     .then(() => {
+  //       setUrl(`https://anasdevbucket.s3.eu-north-1.amazonaws.com/data.json`);
+  //       //critical part end check in Firefox if CORS fails
+  //       console.log(`File uploaded successfully at ${url}`);
 
-        toast.success("File uploaded successfully.", {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          theme: "dark",
-        });
-      })
-      .catch((err: AWS.AWSError) => {
-        console.log(err);
-        alert("Error uploading file.");
-      });
+  //       toast.success("File uploaded successfully.", {
+  //         position: "top-center",
+  //         autoClose: 5000,
+  //         hideProgressBar: false,
+  //         closeOnClick: true,
+  //         pauseOnHover: true,
+  //         draggable: true,
+  //         theme: "dark",
+  //       });
+  //     })
+  //     .catch((err: AWS.AWSError) => {
+  //       console.log(err);
+  //       alert("Error uploading file.");
+  //     });
+  // };
+  // // Function to handle file and store it to file state
+  // const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const file = e.target.files![0];
+  //   setFile({ name: file.name, body: file });
+  // };
+  // // s3 upload end
+  const handelSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e);
+    // uploadFile();
   };
-  // Function to handle file and store it to file state
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files![0];
-    setFile({ name: file.name, body: file });
-  };
-  // s3 upload end
+  console.log(import.meta.env.VITE_API);
+
+  // console.log(process.env.REACT_APP_DESCRIPTION);
 
   return (
     <>
       <div className="App">
         <h1>JSON Data</h1>
-        {data && (
-          <ul>
-            <li>Name: {data.name}</li>
-            <li>Age: {data.balance}</li>
-            <li>City: {data.city}</li>
-          </ul>
+        {data === null ? (
+          <p>loading...</p>
+        ) : (
+          data &&
+          data.map((item, index) => (
+            <ul key={index}>
+              <li>User name: {item.name}</li>
+              <li>Balance: {item.count}</li>
+              <li>Service: {item.serivces}</li>
+            </ul>
+          ))
         )}
+        <Form onClick={handelSubmit} />
       </div>
-      {/* <p>{process.env.REACT_APP_SECRET_NAME}</p> */}
-      <Form onClick={uploadFile} />
     </>
   );
 }
