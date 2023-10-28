b88a6c2f36f3f9875dc68ee9fe32550d3f5a89b0
anas599 <anas599@yahoo.com>
2023-10-28 12:49:13 +0300

    add node server test

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, this is the root of the application.");
+});
+
+app.get("/jokes/random", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://secsers.com/api/v2?key=34cdf3fc90e8331d5a727978619bfb09&action=balance"
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ type: "error", message: error.message });
+  }
+});
+
+const PORT = 3000;
+// const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v2': {
+        target: 'https://secsers.com', // Replace with your API's base URL
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
