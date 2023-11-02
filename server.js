5d63fa4649c7aa059144ef28d18b20b0df2423af
anas599 <anas599@yahoo.com>
2023-11-02 23:10:20 +0300

    add server 2

--- server.js
+++ server.js
@@ -7,15 +7,11 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-app.post("https://secsers.com/api/v2,");
-app.get("/", (req, res) => {
-  res.send("Hello, this is the root of the application.");
-});
 
-app.get("/jokes/random", async (req, res) => {
+app.get("/api/balance", async (req, res) => {
   try {
     const response = await axios.get(
-      "https://secsers.com/api/v2?key=34cdf3fc90e8331d5a727978619bfb09&action=balance"
+      "https://secsers.com/api/v2?key=14f1aa3a1d3e6872069740f71c3a2468&action=balance"
     );
     res.json(response.data);
   } catch (error) {
@@ -26,3 +22,4 @@ app.get("/jokes/random", async (req, res) => {
 const PORT = 3000;
 // const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+// "https://secsers.com/api/v2?key=14f1aa3a1d3e6872069740f71c3a2468&action=balance";
---
+++ server2.js
@@ -0,0 +1,44 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import "dotenv/config";
+const app = express();
+// eslint-disable-next-line no-undef
+const key = process.env.API_KEY;
+const baseUrl = "https://secsers.com/api/v2";
+const PORT = 3001;
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.use(cors());
+app.get("/api/balance", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://secsers.com/api/v2?key=${key}&action=balance`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ type: "error", message: error.message });
+  }
+});
+
+app.post("/api/postLink", async (req, res) => {
+  const link = req.body.link;
+  const quantity = req.body.quantity;
+  const url = `${baseUrl}?key=${key}&action=add&service=4801&link=${link}&quantity=${quantity}`;
+
+  console.log(url);
+  try {
+    const response = await axios.post(url);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ type: "error", message: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
